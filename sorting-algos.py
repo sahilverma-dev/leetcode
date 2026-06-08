a620d62342193219902f19af06c34d35fdf03d7b
Sahil Verma <domainsahil007@gmail.com>
2026-06-08 12:23:31 +0530

    insertion sort

--- sorting-algos.py
+++ sorting-algos.py
@@ -1,15 +1,27 @@
 from typing import List
 
 
-a: List[int] = [5, 2, 9, 1, 7]
+arr = [5, 2, 9, 1, 7]
 
-n = len(a)
+
+n = len(arr)
 # buddle sort
 
-for i in range(n):
-    for j in range(0, n - i - 1):
-        if a[j] > a[j + 1]:
-            a[j], a[j + 1] = a[j + 1], a[j]
-        # print(a[i], a[j])
+# for i in range(n):
+#     for j in range(0, n - i - 1):
+#         if a[j] > a[j + 1]:
+#             a[j], a[j + 1] = a[j + 1], a[j]
+# print(a[i], a[j])
 
-print(a)
+# insertion sort
+for i in range(1, n - 1):
+    key = arr[i]
+    j = i - 1
+
+    while j >= 0 and arr[j] > key:
+        arr[j + 1] = arr[j]
+        j -= 1
+
+    arr[j + 1] = key
+
+print(arr)
