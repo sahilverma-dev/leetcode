01de44449f6a01806a91123967cfa9af26fdcef6
Sahil Verma <domainsahil007@gmail.com>
2026-06-08 12:45:57 +0530

    selection sort

--- sorting-algos.py
+++ sorting-algos.py
@@ -14,14 +14,24 @@ n = len(arr)
 # print(a[i], a[j])
 
 # insertion sort
-for i in range(1, n - 1):
-    key = arr[i]
-    j = i - 1
+# for i in range(1, n - 1):
+#     key = arr[i]
+#     j = i - 1
 
-    while j >= 0 and arr[j] > key:
-        arr[j + 1] = arr[j]
-        j -= 1
+#     while j >= 0 and arr[j] > key:
+#         arr[j + 1] = arr[j]
+#         j -= 1
 
-    arr[j + 1] = key
+#     arr[j + 1] = key
+
+# selection sort
+for i in range(n):
+    min_index = i
+    for j in range(i + 1, n):
+        if arr[j] < arr[min_index]:
+            min_index = j
+
+    arr[i], arr[min_index] = arr[min_index], arr[i]
+
 
 print(arr)
